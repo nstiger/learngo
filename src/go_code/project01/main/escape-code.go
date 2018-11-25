9ee7fe74401bc26b4665ca3712504427cc97c16d
nstiger <andrewong1007@gmail.com>
2018-11-25 17:42:06 +0800

    --nomessage

--- src/go_code/project01/main/escape-code.go
+++ src/go_code/project01/main/escape-code.go
@@ -2,6 +2,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("姓名\t年龄\t籍贯\t住址\n")
-	fmt.Println("jack\t33\t北京\t深圳\r")
+	fmt.Println("姓名\t年龄\t籍贯\t住址\njack\t33\t北京市昌平区沙河镇\t深圳\r")
+	// fmt.Println("")
 }
