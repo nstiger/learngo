57daf1e97d589c9e4e6bb379e7500a47466361eb
nstiger <andrewong1007@gmail.com>
2019-01-08 18:09:09 +0800

    -- doublelink CRUD

---
+++ src/go_code/chapter20/singlelink/main.go
@@ -0,0 +1,143 @@
+/*
+单链表的应用实例
+使用带head头的单向链表实现-水浒英雄排行榜的管理
+完成对英雄人物的增删改查操作
+*/
+package main
+import (
+	"fmt"
+)
+
+//定义一个HeroNode
+type HeroNode struct {
+	no int
+	name string
+	nickname string
+	next *HeroNode	//这个表示指向下一个结点
+}
+
+//给链表插入一个结点
+// 编写第一种插入方式，在单链表的最后加入
+func InsertHeroNode(head *HeroNode, newHeroNode * HeroNode) {
+	//思路
+	//1.先找到该链表的最后这个结点
+	//2.创建一个辅助结点
+	temp := head
+	for {
+		if temp.next == nil { 	//表示找到最后
+			break
+		}
+		temp = temp.next 	//让temp不断地指向下一个结点
+	}
+	//3.将newHeroNode加入到链表的最后
+	temp.next = newHeroNode
+}
+
+//给链表插入一个结点
+// 编写第2种插入方式，根据no的编号从小到大插入
+func InsertHeroNode2(head *HeroNode, newHeroNode * HeroNode) {
+	//思路
+	//1.找到适当的结点
+	//2.创建一个辅助结点
+	temp := head
+	flag := true 	//表示能不能插入，默认为可以
+	//让插入的结点的no，和temp的下一个结点的no比较
+	for {
+		if temp.next == nil { 	//说明到了链表的最后
+			break
+		} else if temp.next.no > newHeroNode.no {
+			//说明newHeroNode就应该插入到temp后面
+			break
+		} else if temp.next.no == newHeroNode.no {
+			//说明链表中已经有了这个no， 不允许插入
+			flag = false
+			break
+		}
+		temp = temp.next
+	}
+	if !flag {
+		fmt.Println("对不起，已经存在no=", newHeroNode.no)
+		return
+	} else {
+		newHeroNode.next = temp.next
+		temp.next = newHeroNode
+	}
+}
+
+func DelHeroNode(head *HeroNode, id int) {
+	temp := head
+	flag := false 	//假设不能找到
+	//找到要删除的结点的no，和temp的下一个结点的no比较
+	for {
+		if temp.next == nil { 	//说明到了链表的最后
+			break
+		} else if temp.next.no == id {
+			//说明我们找到了
+			flag = true
+			break
+		}
+		temp = temp.next
+	}
+	if flag {	//找到，删除
+		temp.next = temp.next.next
+	} else {
+		fmt.Println("sorry，要删除的id不存在")
+	}
+
+}
+
+//显示链表的所有结点信息
+func ListHeroNode(head *HeroNode) {
+	//1.创建一个辅助结点
+	temp := head
+	//先判断该链表是不是一个空的链表
+	if temp.next == nil {
+		fmt.Println("空空如也......")
+		return
+	}
+	//2.遍历这个链表
+	for {
+		fmt.Printf("[%d, %s, %s]==>", temp.next.no, temp.next.name, temp.next.nickname )
+		//判断是否链表最后
+		temp = temp.next
+		if temp.next == nil {
+			break
+		}
+	}
+}
+
+func main() {
+	//1.先创建一个头结点
+	head := &HeroNode{}
+
+	//2.创建一个新的HeroNode
+	hero1 := &HeroNode{
+		no : 1,
+		name : "宋江",
+		nickname : "及时雨",
+	}
+	hero2 := &HeroNode{
+		no : 2,
+		name : "卢俊义",
+		nickname : "玉麒麟",
+	}
+	hero3 := &HeroNode{
+		no : 3,
+		name : "林冲",
+		nickname : "豹子头",
+	}
+
+
+	//3.加入
+	InsertHeroNode2(head, hero3)
+	InsertHeroNode2(head, hero1)
+	InsertHeroNode2(head, hero2)
+
+	//4.显示
+	ListHeroNode(head)
+
+	//5.删除
+	fmt.Println()
+	DelHeroNode(head, 2)
+	ListHeroNode(head)
+}
