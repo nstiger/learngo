3fa6387056adf5a7270a7e8a736c06c7d430f65f
nstiger <andrewong1007@gmail.com>
2019-02-02 11:46:50 +0800

    --plan.go

---
+++ src/go_code/plan.go
@@ -0,0 +1,47 @@
+/**/
+
+package main
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println()
+	fmt.Println("--------2019年新春菜单----------")
+	fmt.Println("大年三十 ———— 早餐：牛肉米粉")
+	fmt.Println("中餐：汉堡、沙拉、牛奶")
+	fmt.Println("晚餐：北京烤dk、四喜丸子、吉祥有鱼、宫爆鸡丁、粉蒸排骨、韭菜鸡蛋")
+	fmt.Println("      梅菜扣肉、腊味合蒸、麻辣牛肉、醋溜白菜、清炒藕丁、麻婆豆腐")
+	fmt.Println()
+	fmt.Println("-------------------------新年吉祥分割线--------------------------")
+	fmt.Println()
+	fmt.Println("正月初一 ———— 观看春晚，无法起床，早餐省略")
+	fmt.Println("中餐：昨天的剩菜")
+	fmt.Println("晚餐：昨天的剩菜")
+	fmt.Println()
+	fmt.Println("正月初二 ———— 为国熬夜，无法起床，早餐省略")
+	fmt.Println("中餐：前天的剩菜")
+	fmt.Println("晚餐：前天的剩菜")
+	fmt.Println()
+	fmt.Println("正月初三 ———— 为国熬夜，无法起床，早餐省略")
+	fmt.Println("中餐：速食饺子")
+	fmt.Println("晚餐：四川火锅")
+	fmt.Println()
+	fmt.Println("正月初四 ———— 为国熬夜，无法起床，早餐省略")
+	fmt.Println("中餐：KFC全家桶")
+	fmt.Println("晚餐：昨天的四川火锅")
+	fmt.Println()
+	fmt.Println("正月初五 ———— 为国熬夜，无法起床，早餐省略")
+	fmt.Println("中餐：康师傅套餐")
+	fmt.Println("晚餐：参照工作日，四菜一汤")
+	fmt.Println()
+	fmt.Println("正月初六 ———— 为国熬夜，无法起床，早餐省略")
+	fmt.Println("中餐：金拱门麦克唐纳全家桶")
+	fmt.Println("晚餐：参照工作日，四菜一汤")
+	fmt.Println()
+	fmt.Println("正月初七 ———— 为国熬夜，无法起床，早餐省略")
+	fmt.Println("中餐：清水面条")
+	fmt.Println("晚餐：在老北京火锅店就餐，欢送dk")
+	fmt.Println("---------------------------新春佳节圆满顺利-----------------------")
+
+}
